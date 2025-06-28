8057d8727df9ef08dfb2fb1d8a45d588146f74d7
daviegito <daviegitocoelho@gmail.com>
2025-06-28 12:25:06 -0300

    feat: mais adições ao lexer, mas rever alguns pontos antes

--- mini-interpretador/src/ast/lexer.rs
+++ mini-interpretador/src/ast/lexer.rs
@@ -1,4 +1,3 @@
-
 pub enum TokenKind {
     Number(i64),
     Mais,
@@ -7,6 +6,8 @@ pub enum TokenKind {
     Barra,
     EsqPar,
     DirPar,
+    Indef,
+    EOF
 }
 
 pub struct TextSpan {
@@ -37,4 +38,69 @@ impl Token {
 
 }
 
+pub struct Lexer<'a> {
+    input: &'a str,
+    current_pos: usize,
+}
 
+implo <'a> Lexer<'a> {
+    
+    pub fn new(input: &'a str) -> Self {
+        Self { input, current_pos: 0 } 
+    }
+
+    pub fn next_token(&mut self) -> Option<Token> {
+        if self.current_pos > self.input.len() {
+            return None;
+        }
+        if self.current_pos == self.input.len() {
+            let end_of_file_char: char = '\0';
+            self.current_pos += 1;
+            return Token::new(
+                TokenKind::EOF,
+                TextSpan::new(0,0,end_of_file_char.to_string()) 
+            );
+        }
+        
+        let start: = self.current_pos;
+        let c = self.current_char();
+        let mut kind = TokenKind::Indef;
+        if Self::is_number_start(%c) {
+           let number: i64 = self.consume_number();
+           kind = TokenKind::Number(number);
+        }
+        let end = self.current_pos;
+        let literal = self.input[start..end].to_string();
+        let span = TextSpan::new(start, end, literal);
+
+    }
+    
+    fn is_number_start(c: &char) -> bool {
+        c.is_digit(10)
+    }
+
+    fn current_char(&self) -> char {
+        self.input.chars().nth(self.current_pos).unwrap()
+    }
+
+    fn consume(&mut self) -> Option<char> {
+        let c = self.current_char();
+        self.current_pos += 1;
+        if self.current_pos >= self.input.len() {
+            return None;
+        }
+        Some(c)
+    }
+
+    fn consume_number(&mut self) -> i64 {
+        let mut number: i64 = 0;
+        while let Some(c) = self.consume() {
+            if c.is_digit(10) {
+                number = number * 10 + c.to_digit(10).unwrap() as i64;
+            } else {
+                break;
+            }
+        }
+        number 
+    }
+}
--- mini-interpretador/src/main.rs
+++ mini-interpretador/src/main.rs
@@ -3,7 +3,7 @@ mod ast;
 fn main() {
     println!("Olá. Este é um mini interpretador de expressões aritméticas em Rust");
     println!("Digite uma expressão aritmética abaixo, como: '3 + (4 * 2)':");
-    
+        
     //Em caso de sucesso - input
     println!("A expressão que você digitou é válida!");
     println!("Ela retorna...");
